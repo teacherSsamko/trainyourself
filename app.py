8c23054461f26a737f49118fe704b8b9c65ccabf
ssamko <teacher.ssamko@gmail.com>
2020-06-15 23:17:01 +0900

    fix: location을 lat, lon으로 나눠서 저장

--- app.py
+++ app.py
@@ -8,33 +8,44 @@ CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 # client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 client = MongoClient('mongodb://ssamko:dmstjq@34.64.213.249', 27017)
-db = client.DBdev 
+db = client.DBdev
 # db = client.spots
+
 
 @app.route('/')
 def home():
-   return 'This is Home!'
+    return 'This is Home!'
+
 
 @app.route('/spots/new', methods=['POST'])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def newSpotAPI():
-    location_receive = request.form['location_give']
+    lat_receive = request.form['lat_give']
+    lon_receive = request.form['lon_give']
     pullUp_receive = request.form['pullUp_give']
     parallel_receive = request.form['parallel_give']
     etc_receive = request.form['etc_give']
 
     spot = {
-        'location': location_receive,
+        'lat': lat_receive,
+        'lon': lon_receive,
         'pullUp': pullUp_receive,
         'parallel': parallel_receive,
         'etc': etc_receive
     }
-    
+
     print(spot)
 
     db.trainyourself.insert_one(spot)
-    
-    return jsonify({'result': 'success', 'msg':'등록되었습니다.'})
 
-if __name__ == '__main__':  
-   app.run('0.0.0.0', port=5000, debug=True)
+    return jsonify({'result': 'success', 'msg': '등록되었습니다.'})
+
+
+@app.route('/spots', methods=['GET'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+def findSpotAPI():
+    pass
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
