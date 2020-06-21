ea6ad808d5a82771381e98e3fdf98305aca917d2
ssamko <teacher.ssamko@gmail.com>
2020-06-21 11:34:03 +0900

    feat: some changes

---
+++ trainyourself/trainyourself/settings_/development.py
@@ -0,0 +1,18 @@
+from .base import *
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"), # Root의 static 파일
+
+)
